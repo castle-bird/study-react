7ffb93ec0fc1dc034d087dd881d3f2089ddd6002
seongjo0501 <seongjo0501@gmail.com>
2024-10-18 22:19:15 +0900

    api테스트

--- 02-notion-api-test/vite.config.js
+++ 02-notion-api-test/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.notion.com', // Notion API의 기본 URL
+        changeOrigin: true,
+        secure: false, // HTTPS 환경에서 SSL 검사 비활성화
+        rewrite: (path) => path.replace(/^\/api/, '') // '/api' 경로를 제거
+      }
+    }
+  }
+});
